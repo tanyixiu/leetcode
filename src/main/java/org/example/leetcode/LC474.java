ff2b4a50d0327d2493a52092cdd4cbc4b893a7fb
yxtan <yixiu.tan@gmail.com>
2024-06-06 16:10:36 +0800

    [YIXIU] refactor lc474

--- src/main/java/org/example/leetcode/LC474.java
+++ src/main/java/org/example/leetcode/LC474.java
@@ -21,17 +21,18 @@ public class LC474 {
     int findMaxForm(String[] strs, int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i <= strs.length; i++) {
-            int[] si = get01(strs[i - 1]);
-            int zeros = si[0];
-            int ones = si[1];
-            for (int j = m; j >= zeros; j--) {
-                for (int k = n; k >= ones; k--) {
-                    dp[j][k] = Math.max(dp[j][k], dp[j - zeros][k - ones] + 1);
+
+            int[] vi_01 = get01(strs[i - 1]);
+
+            for (int j = m; j >= vi_01[0]; j--) {
+                for (int k = n; k >= vi_01[1]; k--) {
+                    dp[j][k] = Math.max(dp[j][k], dp[j - vi_01[0]][k - vi_01[1]] + 1);
                 }
             }
         }
         return dp[m][n];
     }
+
 
     int[] get01(String s) {
         int zeros = 0;
@@ -39,35 +40,41 @@ public class LC474 {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '0') {
                 zeros++;
-            } else {
+            } else if (s.charAt(i) == '1') {
                 ones++;
             }
         }
-        int[] my01 = new int[2];
-        my01[0] = zeros;
-        my01[1] = ones;
-        return my01;
+        return new int[] {zeros, ones};
     }
 
 }
 
+
 /*
  * thinking:
+ * 1. 找到确定的容量边界: m 和 n，有两个
  *
  * dp[i][j][k]:
  * i -- 表示从1-i个字符串中选择任意个
- * j -- 使得其中 0的个数不超过j
- * k -- 使得其中 1的个数不超过k
- * v -- 得到的最大子集个数
+ * j -- 使得其中其0的个数不超过j, j<=m
+ * k -- 使得其中其1的个数不超过k, k<=n
+ * v -- 满足情况的最大子集的长度
  *
- * when 不能选 i --> 选了 i 就会超 --> strs[i].zeros > j || strs[i].ones > k
+ * when 不能选 i (何时不能选? --> 选了 i 就超过了容量 --> i.zeros > j || i.ones > k)
  * 则 dp[i][j][k] = dp[i-1][j][k]
  *
  * when 可以选 i
- * 则 dp[i-1][j][k] 或者 dp[i][j-strs[i].zeros][k-strs[i].ones]+1
- * 选 i 最大个数是 x
- * 不选 i 最大个数 y
- * 选i 和不选i都可以满足要求，那就 max(x,y+1)
+ * 方案1: 不选 i,则 --> dp[i-1][j][k]
+ * 方案2: 要选 i,则 --> dp[i-1][j-i.zeros][k-i.ones] + 1 --> 不选 i 时的情况+1
+ * 比较: 选个数最多的方案
+ * 则: dp[i][j] = max(dp[i-1][j] , dp[i-1][j-i.zeros][k-i.ones] + 1)
  *
-
+ * 初始化 dp[0][j][k]/dp[i][0][k]/dp[i][j][0]:
+ * --> dp[0][j][k]: 一个都不选，只有dp[0][0][0] = 0，其他的看情况
+ * --> dp[i][0][k]: 0 个 0，除非全是 1，也是看情况
+ * --> dp[i][j][0]: 0 个 1，除非全是 0，也是看情况
+ *
+ * 则滚动数组:
+ * dp[j][k] = max(dp[j][k] , dp[j-i.zeros][k-i.ones] + 1)
+ * j, k 倒序遍历
  */
